43dfca441a3e007007cd140b948adaa8c1bd1df3
Daniel Marchi <marchimail@gmail.com>
2020-08-15 15:03:32 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrumFusca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
