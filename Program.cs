8a4e0b6dc43e23dd9ea35b2503b4a257df6207ce
Daniel Marchi <marchimail@gmail.com>
2020-08-15 15:45:31 -0300

    Instalar Virabrequim

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-
-namespace ScrumFusca
+﻿namespace ScrumFusca
 {
     class Program
     {
         static void Main(string[] args)
         {
             var cilindro = new Cilindro();
+            var virabrequim = new Virabrequim();
 
             cilindro.Montar();
+            virabrequim.Instalar();
         }
     }
 }
---
+++ Virabrequim.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ScrumFusca
+{
+    public class Virabrequim
+    {
+        public void Instalar()
+        {
+            Console.WriteLine("* Virabrequim instalado");
+        }
+    }
+}
