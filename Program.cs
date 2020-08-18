69d4b334ca7346ba2fbfe8b2dfd9f2ae97aaa951
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-18 09:05:43 -0300

    resolvendo merge

--- Program.cs
+++ Program.cs
@@ -6,21 +6,15 @@
         {
             var bateria = new Bateria();
             var cilindro = new Cilindro();
- 
             var suspensao = new Suspensao();
             var tanque = new TanqueGasolina();
             var virabrequim = new Virabrequim();
-<<<<<<< HEAD
-            var bateria = new Bateria();
             var cambio = new Cambio();
-            
-            cambio.Montar();
-=======
             var valvulas = new Valvulas();
             var velas = new Velas();
 
+            cambio.Montar();
             tanque.Montar();
->>>>>>> 85b2fc361c07a757f90ce2f35b7b1031d4ac835b
             cilindro.Montar();
             suspensao.Montar();
             virabrequim.Instalar();
@@ -34,16 +28,6 @@
             ie.Ligar();
             System.Threading.Thread.Sleep(100);
             ie.Desligar();
-<<<<<<< HEAD
-
-
-            
-            
-            
-           
-
-=======
->>>>>>> 85b2fc361c07a757f90ce2f35b7b1031d4ac835b
         }
 
     }
